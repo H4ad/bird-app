ab19694b15d82f085367365c3b324c43f0376a3e
Vinícius Lourenço <vinyygamerlol@gmail.com>
2020-07-27 22:21:03 -0300

    refactor(my-comments): added comments and constructor to page

--- app/src/app/pages/main/my-comments/my-comments.page.ts
+++ app/src/app/pages/main/my-comments/my-comments.page.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+//#region Imports
 
+import { Component } from '@angular/core';
+
+//#endregion
+
+/**
+ * A classe que representa a página que lista todos os meus comentários criados
+ */
 @Component({
-  selector: 'app-my-comments',
+  selector: 'bird-my-comments',
   templateUrl: './my-comments.page.html',
   styleUrls: ['./my-comments.page.scss'],
 })
-export class MyCommentsPage implements OnInit {
+export class MyCommentsPage {
 
+  //#region Constructor
+
+  /**
+   * Construtor padrão
+   */
   constructor() { }
 
-  ngOnInit() {
-  }
+  //#endregion
 
 }
