12832255af3bf1e925a8888840dce0fa751534f6
Vinícius Lourenço <vinyygamerlol@gmail.com>
2020-07-28 00:18:43 -0300

    feat(environment): added initial configuration for api

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -7,4 +7,10 @@ export const environment = {
   keys: {
     myComments: 'MY_COMMENTS_KEY',
   },
+  api: {
+    baseUrl: 'http://localhost:3000',
+    comment: {
+      list: '/comments?page={currentPage}&maxItens={maxItens}',
+    },
+  },
 };
--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -11,6 +11,12 @@ export const environment = {
   keys: {
     myComments: 'MY_COMMENTS_KEY',
   },
+  api: {
+    baseUrl: 'http://localhost:3000',
+    comment: {
+      list: '/comments?page={currentPage}&maxItens={maxItens}',
+    },
+  },
 };
 
 /*
